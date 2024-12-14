7aac5cd932524d52fb1da958e62e414c764c35e9
Arun Kumar <arunprajapati9825@gmail.com>
2024-12-14 14:03:13 +0530

    Enhance MyProjectList component with improved hover styles for sidebar menu items

--- src/app/(dashboard)/components/MyProjectList.tsx
+++ src/app/(dashboard)/components/MyProjectList.tsx
@@ -19,13 +19,17 @@ export function MyProjectList({
 		<SidebarMenu>
 			{options.map((option) => (
 				<SidebarMenuItem key={option.path}>
-					<SidebarMenuButton asChild tooltip={option.title}>
+					<SidebarMenuButton
+						asChild
+						tooltip={option.title}
+						className="hover:bg-inherit"
+					>
 						<Link
 							href={option.path}
 							className={cn(
 								"hover:dark:bg-slate-500 hover:bg-slate-200 text-zinc-500 dark:text-zinc-200",
 								pathname === option.path &&
-									"hover:bg-blue-600 hover:dark:text-slate-200 hover:text-slate-200 bg-primary dark:text-slate-50 text-primary-foreground hover:dark:bg-primary/90"
+									"sm:hover:bg-blue-600 hover:bg-blue-600 hover:dark:text-slate-200 hover:text-slate-200 bg-primary dark:text-slate-50 text-primary-foreground hover:dark:bg-primary/90"
 							)}
 						>
 							<div className={cn("rounded-full")}>
